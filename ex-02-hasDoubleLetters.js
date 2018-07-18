d071ad5bc1a8aee13f900ccd62c6fa7daa0c6830
Herdez <abelquantikum@gmail.com>
2018-07-18 12:57:19 -0500

    assignment js iteration intermediate

---
+++ ex-02-hasDoubleLetters.js
@@ -0,0 +1,62 @@
+/**
+ * hasDoubleLetters()
+ *
+ * Write a function called `hasDoubleLetters` that takes a string as an argument
+ * and returns a boolean if it has a word with double letters.
+ *
+ *    hasDoubleLetters('matter')
+ *     => true // the tt are double-letters
+ *
+ *    hasDoubleLetters('chopper')
+ *       => true // the pp are double-letters
+ *
+ *    hasDoubleLetters('mister')
+ *      => false  // no double letters
+ *
+ *    hasDoubleLetters('demand')
+ *      => false // the double-letters have to be next to eachother
+**/
+
+// ++ YOUR CODE below
+
+function hasDoubleLetters(str){
+	for (var i = 0; i < str.length - 1; i++){
+		strChar1 = str[i].toLowerCase();
+		strChar2 = str[i + 1].toLowerCase();
+		if (strChar1 === strChar2){
+			return true;
+		}
+	}
+	return false;
+}
+
+
+console.log(hasDoubleLetters("Aardvark"));
+
+
+
+
+
+
+// *~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*
+// *~*~*~*~*~*~*~* Tests (Don't Touch) *~*~*~*~*~*~*~*~*
+// *~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*
+
+console.group('ex-02');
+  console.log('%cFunction: hasDoubleLetters', 'background-color: green; color: white')
+console.groupEnd();
+
+  console.log('Should return true for arg input "shutter"');
+  console.assert(hasDoubleLetters('stutter') === true)
+
+  console.log('Should return false for arg input "prospect"');
+  console.assert(hasDoubleLetters('prospect') === false)
+
+
+  console.log('Should return false for arg input "shoehorn"');
+  console.assert(hasDoubleLetters('shoehorn') === false)
+
+  console.log('Should return true for arg input "Aardvark"');
+  console.assert(hasDoubleLetters('Aardvark') === true)
+
+console.log('\n')
